c885699ea814156a3b2f40b236e47b8633306421
JINO-ROHIT <find.jinorohit@gmail.com>
2024-08-25 14:19:58 +0530

    multiple threads in a list

---
+++ 07_multiplethreads.py
@@ -0,0 +1,24 @@
+'''
+LIST OF MULTIPLE THREADS
+'''
+
+import time
+import threading
+
+
+
+def do_task(index):
+    time.sleep(0.5)
+    print(index, end='')
+
+
+
+NUM_THREADS = 5
+lstth = []
+
+for i in range(NUM_THREADS):
+    lstth.append(threading.Thread( target=do_task, args=(i,) ))
+
+for th in lstth:
+    th.start()
+    #th.join()
